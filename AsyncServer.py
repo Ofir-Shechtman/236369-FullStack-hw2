bdb6481ff4b9dd2105d5b7756fa3dbb3e429760f
ofir511996 <ofir511996@gmail.com>
2021-11-21 09:52:29 +0200

    add Authorization with DB

--- AsyncServer.py
+++ AsyncServer.py
@@ -1,5 +1,7 @@
 from aiohttp import web, BasicAuth
 import config
+import Users
+
 
 async def readable_file(request):
     return web.FileResponse(status=200, path='text.txt',
@@ -12,13 +14,29 @@ async def middleware(request, handler):
     if auth_header:
         auth = BasicAuth.decode(auth_header)
         print(auth)
-        if auth.login == 'ofir' and auth.password == '123':
+        user = app['database'].select(auth.login)
+        print(user)
+        if user and user.password == auth.password:
             print(f'{auth.login} is Authorized')
             return await handler(request)
     response = web.Response(status=401, headers={"WWW-Authenticate": 'Basic realm="hw2-realm"'})
     return response
 
+
+async def connect_db(app):
+    print("Connecting DB...")
+    app['database'] = Users.Users()
+    print(app['database'])
+
+
+async def disconnect_db(app):
+    app['database'].close()
+    print("DB closed")
+
+
 if __name__ == '__main__':
     app = web.Application(middlewares=[middleware])
+    app.on_startup.append(connect_db)
+    app.on_cleanup.append(disconnect_db)
     app.router.add_get('/', readable_file)
     web.run_app(app, host="localhost", port=config.port, shutdown_timeout=config.timeout)
--- Users.py
+++ Users.py
@@ -2,14 +2,15 @@ import sqlite3
 from dataclasses import dataclass
 from typing import Union, List
 
+
 @dataclass(frozen=True)
 class User:
-     username: str
-     password: str
+    username: str
+    password: str
 
 
 class Users:
-    def __init__(self, auto_commit: bool=True, db_file: str='users.db'):
+    def __init__(self, auto_commit: bool = True, db_file: str = 'users.db'):
         self.__connection = sqlite3.connect(db_file)
         self.__cursor = self.__connection.cursor()
         self.__auto_commit = auto_commit
@@ -21,7 +22,7 @@ class Users:
 
     def __enter__(self):
         return self
-    
+
     def __exit__(self, type, value, traceback):
         self.close()
 
@@ -34,50 +35,47 @@ class Users:
     def insert(self, *args):
         """insert(self, user: User)
            insert(self, username: str,password: str)"""
-        if len(args)==1 and isinstance(args[0], User):
-            self.__cursor.execute("insert into Users(username,password) values (?, ?)", (args[0].username, args[0].password))
-        elif len(args)==2:
+        if len(args) == 1 and isinstance(args[0], User):
+            self.__cursor.execute("insert into Users(username,password) values (?, ?)",
+                                  (args[0].username, args[0].password))
+        elif len(args) == 2:
             self.__cursor.execute("insert into Users(username,password) values (?, ?)", args)
         else:
             raise NotImplementedError()
 
-    def delete(self, username:str) -> int:
+    def delete(self, username: str) -> int:
         return self.__cursor.execute("delete from Users where username=?", (username,)).rowcount
 
     def select(self, username: str) -> Union[User, bool]:
         self.__cursor.execute("select password from Users where username=?", (username,))
         password = self.__cursor.fetchone()
         if password:
-            return User(username, password)
+            return User(username, password[0])
         else:
             return False
 
-    def delete_all(self) ->int:
+    def delete_all(self) -> int:
         return self.__cursor.execute("delete from Users").rowcount
 
     def select_all(self) -> List[User]:
-        self.__cursor.execute("select username, password from Users")
+        self.__cursor.execute("select username, password from Users order by lower(username)")
         return list(map(lambda x: User(*x), self.__cursor.fetchall()))
 
     def __repr__(self) -> str:
-        data = users.select_all()
+        data = self.select_all()
         if not data:
             return ''
         s = 'Users:\n'
-        for u in users.select_all():
-            s+=str(u)+'\n'
+        for u in self.select_all():
+            s += str(u) + '\n'
         return s
 
 
-    
-
-with Users() as users:
-    print(users)
-    users.delete_all()
-    users.insert('Ofir', '1234')
-    users.insert(User('admin', '0000'))
-    print(users)
-    users.rollback()
-    
-
-
+if __name__ == '__main__':
+    with Users() as users:
+        print(users)
+        users.delete_all()
+        users.insert('Ofir', '1234')
+        users.insert(User('admin', '0000'))
+        print(users)
+        users.rollback()
